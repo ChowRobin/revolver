dafa698607b73c62f03570f6099e5b3f25dd04e6
robin <robinchow8991@gmail.com>
2018-06-08 23:02:40 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,4 @@
+## Revolver
+### 介绍
+Revovler基于springboot，学习开源项目Guns，用于整合学习过的知识。
+### 实现功能
---
+++ src/main/java/pers/robin/revolver/RevolverApplication.java
@@ -0,0 +1,12 @@
+package pers.robin.revolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevolverApplication.class, args);
+    }
+}
